61ec03dee14cff7553ceda001999ea970696500d
nguyenvandai61 <nguyenvandai61@users.noreply.github.com>
2021-04-21 19:26:04 +0700

    Only admin access path 'admin/'

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -1,16 +1,24 @@
 import Layout, { Content } from 'antd/lib/layout/layout';
+import { useState, useEffect } from 'react';
+import { Redirect } from 'react-router';
+import { readCookie } from 'utils/cookie';
 import AdminSider from './AdminSider';
 function AdminLayout(props) {
-    const {siderRoutes, siderRouter} = props;
+    const [authFails, setAuthFails] = useState((readCookie('access_token')===''));
+    const { siderRoutes, siderRouter } = props;
+    console.log(authFails);
+    if (authFails) {
+        return <Redirect to='/login' />;
+    }
     return (
-        <Layout style={{flexDirection: 'row' }}>
+        <Layout style={{ flexDirection: 'row' }}>
             <AdminSider routes={siderRoutes}></AdminSider>
             <Layout style={{
                 height: '100vh',
                 margin: 0,
             }}>
                 {/* <CustomHeader ></CustomHeader> */}
-                <Content style={{ height: '100%', textAlign: 'left', margin:'15px'}}>
+                <Content style={{ height: '100%', textAlign: 'left', margin: '15px' }}>
                     {siderRouter}
                 </Content>
             </Layout>
--- src/modules/pages/HomePage/index.tsx
+++ src/modules/pages/HomePage/index.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 import ContestListPage from '../ContestListPage/index';
 import ContestsAttendCard from './components/ContestsAttendCard';
 import logOut from 'services/logout';
+import {useState} from 'react';
+import { readCookie } from 'utils/cookie';
 
 export function HomePage () {
+    
     return (
         <Layout style={{flexDirection: 'row' }}> 
             <ContestListPage style={{width: '75%'}}></ContestListPage>

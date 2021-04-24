d4fc64448525db3ef1c3eae95ed4a517eb5acbf7
nguyenvandai61 <nguyenvandai61@users.noreply.github.com>
2021-04-24 11:34:52 +0700

    Custom button typescript

--- src/components/core/CustomButton.tsx
+++ src/components/core/CustomButton.tsx
@@ -1,6 +1,10 @@
 import { Button } from 'antd';
+import { ButtonType } from 'antd/lib/button';
+import { BaseButtonProps } from 'antd/lib/button/button';
+type CustomButtonProps = BaseButtonProps&{
+}
 
-export default function CustomButton(propsObj) {
+export default function CustomButton(propsObj:CustomButtonProps) {
     return (
         <Button {...propsObj} >
         </Button>

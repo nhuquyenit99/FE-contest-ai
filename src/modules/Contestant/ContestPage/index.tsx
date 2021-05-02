d0e86286ab9de0e1d0ebeda79bc6ff451eec084f
nguyenvandai61 <nguyenvandai61@users.noreply.github.com>
2021-05-02 14:15:27 +0700

    Merge

---
+++ src/modules/Contestant/ContestPage/index.tsx
@@ -0,0 +1,30 @@
+import { Card, Tabs } from 'antd';
+import Layout from 'antd/lib/layout/layout';
+import ListProblem from './components/ListProblem';
+import { ProblemContainer } from './components/ProblemContainer/index';
+
+const { TabPane } = Tabs;
+export function ContestPage() {
+    return <>
+        <Layout style={{ flexDirection: 'row' }}>
+            <div style={{ width: '75%', margin: '15px' }}>
+                <Tabs defaultActiveKey="1">
+                    <TabPane tab="Problem" key="1">
+                        <ProblemContainer></ProblemContainer>
+                    </TabPane>
+                    <TabPane tab="Dashboard" key="2">
+                    Content of Tab Pane 2
+                    </TabPane>
+                    <TabPane tab="Sample Code" key="3">
+                        
+                    </TabPane>
+                </Tabs>
+            </div>
+            <div style={{ width: '25%', margin: '15px' }}>
+                <Card>
+                    <ListProblem />
+                </Card>
+            </div>
+        </Layout>
+    </>;
+}

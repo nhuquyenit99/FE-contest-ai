927625f194630b0cad328ed4a6a314cbcdf957a8
nguyenvandai61 <nguyenvandai61@users.noreply.github.com>
2021-05-02 17:09:37 +0700

    Preview sample code help for admin

--- src/modules/Admin/SampleCodePage/index.tsx
+++ src/modules/Admin/SampleCodePage/index.tsx
@@ -1,8 +1,10 @@
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { Button, notification } from 'antd';
+import { Button, Card, notification, Tabs } from 'antd';
 import { useState, useEffect } from 'react';
 import { fetchSampleCodeHelp, fetchUpdateSampleCodeHelp } from 'services/html_document';
+
+const { TabPane } = Tabs;
 
 
 export default function SampleCodePage() {
@@ -14,7 +16,7 @@ export default function SampleCodePage() {
                 setData(resp.html_content);
             });
     }, []);
-    
+
     const handleSaveBtn = () => {
         // let myData = data;
         console.log(data);
@@ -29,28 +31,38 @@ export default function SampleCodePage() {
             }
         );
     };
-
+    const saveButtonItem = <Button id='save' type='primary' onClick={handleSaveBtn}> Save </Button>;
+    
     return <div className='__sample-code-page-container__'>
         <h2>Sample Code</h2>
-        <Button id='save' type='primary' onClick={handleSaveBtn}> Save </Button>
-        <CKEditor
-            editor={ClassicEditor}
-            data={data}
-            onReady={editor => {
-                // You can store the "editor" and use when it is needed.
-                // console.log('Editor is ready to use!', editor);
-            }}
-            onChange={(event, editor) => {
-                const data = editor.getData();
-                setData(data);
-            }}
-            onBlur={(event, editor) => {
-                // console.log('Blur.', editor);
-                // console.log(editor.getData());
-            }}
-            onFocus={(event, editor) => {
-                // console.log('Focus.', editor);
-            }}
-        />
+        <Tabs defaultActiveKey="1" tabBarExtraContent={{right: saveButtonItem}}>
+            <TabPane tab="Edit" key="1">
+                
+                <CKEditor
+                    editor={ClassicEditor}
+                    data={data}
+                    onReady={editor => {
+                        // You can store the "editor" and use when it is needed.
+                        // console.log('Editor is ready to use!', editor);
+                    }}
+                    onChange={(event, editor) => {
+                        const data = editor.getData();
+                        setData(data);
+                    }}
+                    onBlur={(event, editor) => {
+                        // console.log('Blur.', editor);
+                        // console.log(editor.getData());
+                    }}
+                    onFocus={(event, editor) => {
+                        // console.log('Focus.', editor);
+                    }}
+                />
+            </TabPane>
+            <TabPane tab="Preview" key="2">
+                <Card className='__sample-code-container__'>
+                    <div dangerouslySetInnerHTML={ { __html: data } }></div>
+                </Card>
+            </TabPane>
+        </Tabs>
     </div>;
 }

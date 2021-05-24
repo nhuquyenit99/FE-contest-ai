e3c134dd8b84485d7ffaf5060f19c139562f3acd
nguyenvandai61 <nguyenvandai61@users.noreply.github.com>
2021-05-25 03:36:05 +0700

    Full width for list contest

--- src/modules/Contestant/ContestListPage/index.tsx
+++ src/modules/Contestant/ContestListPage/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Skeleton } from 'antd';
+import { Card, List, Skeleton } from 'antd';
 import CardContest from './components/CardContest';
 import { fetchAllContest, ListContests } from 'services/contest';
 export default function ContestListPage(props) {
@@ -17,11 +17,17 @@ export default function ContestListPage(props) {
     }, []);
 
     const renderListContest = () => {
-        return <>
-            {dataList?.map((contest, idx) => 
-                <CardContest key={idx} contest={contest} style={{ marginBottom: '30px' }}></CardContest>
-            )}
-        </>;
+        return <List
+            dataSource={dataList}
+            renderItem={(contest, idx) => {
+                return <List.Item 
+                    style={{display: 'block'}}
+                >
+                    <CardContest key={idx} contest={contest} style={{ marginBottom: '30px' }}></CardContest>
+                </List.Item>;
+            }}
+        >
+        </List>;
     };
     return <>
         <Card key="List contests" {...props} title="List contests">
